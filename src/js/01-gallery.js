1d2fab4bc8ff7b7736a4163b45a976b639b1d9ca
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2024-11-03 18:03:03 +0100

    finish 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+
+
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({preview, original, description})=> 
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+
+).join('')
+
+gallery.insertAdjacentHTML('beforeend', markup)
+new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250
+ })
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+
+const player = new Player('vimeo-player', {
+    id: "vimeo-player",
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+// const onPlay = function(data) {
+//     console.log(data)
+// };
+
+// player.on('play', onPlay);
+
+player.on('timeupdate', function(data) {
+   console.log(data)
+});
 
